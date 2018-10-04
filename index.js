bc0aa7a70053b07ba8c52b4893ee723427537d12
elisa stater <elisa_60@msn.com>
2018-10-04 03:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Joe") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+if (height ==="74") {
+  console.log("The height is correct")
+}
